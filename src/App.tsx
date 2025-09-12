5ad2f2dda9330834b07be328358c8e7439491b0c
lysalum <aglum1@uci.edu>
2025-09-12 04:33:37 -0700

    Exported at right ratio

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import thing from "./assets/frame_blue_hero.png";
 
 import * as htmlToImage from "html-to-image";
 import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
@@ -8,19 +9,30 @@ import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
 function App() {
 	const exportImage = () => {
 		console.log("EXPORTING IANMGE");
-		const node = document.getElementById("thingy") as HTMLElement;
+		const node = document.getElementById("card-img") as HTMLElement;
+        node.style.width = '716px';
+        node.style.height = '1000px';
+        node.style.minWidth = '716px';
+        node.style.minHeight = '1000px';
+        node.style.margin = '0px';
+        node.style.marginLeft = '-1px';
+        node.style.marginTop = '-1px';
 
 		htmlToImage
-			.toPng(node)
+			.toPng(node, {
+                width: 716, 
+                height: 1000,
+            })
 			.then((dataUrl) => {
 				const img = new Image();
 				img.src = dataUrl;
-				document.body.appendChild(img);
+				// document.body.appendChild(img);
 
 				let link = document.createElement("a");
 				link.download = "my-image-name.png";
 				link.href = dataUrl;
 				link.click();
+				document.body.appendChild(img);
 			})
 			.catch((err) => {
 				console.error("oops, something went wrong!", err);
@@ -45,12 +57,14 @@ function App() {
 			</header> */}
 
 			<div className={"page-padding"}>
-                <div className="page-content">
-                    <div className="card-container">
-				<div id="card-img"></div>
-                </div>
-				<button onClick={exportImage}>HERE</button>
-                </div>
+				<div className="page-content">
+					<div className="card-container">
+						<div id="card-img">
+							<img className="img-component" src={thing}></img>
+						</div>
+					</div>
+					<button onClick={exportImage}>HERE</button>
+				</div>
 			</div>
 		</div>
 	);
